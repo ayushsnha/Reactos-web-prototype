cf058a33e4d926e49abcf042a52609c32b93cabf
Ayush Kumar Sinha <ayush_snha@rediffmail.com>
2019-05-21 00:28:49 +0530

    ui

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ if(!dev){
   app.disable('x-powered-by')
   // Serve static files from the React app
   app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-  });
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname+'/client/build/index.html'));
+//   });
 }
 
 
